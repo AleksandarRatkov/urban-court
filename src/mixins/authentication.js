728ede00e496ecdaf970b0aa7432ed7733c2c49f
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-07-16 14:41:55 +0200

    Create autentication mixin

---
+++ src/mixins/authentication.js
@@ -0,0 +1,23 @@
+export default {
+    data() {
+        return {
+            errorMsg: "",
+            isFormSubmitted: false,
+            modals: {
+                classic: false
+            }
+        }
+    },
+    methods: {
+        afterSuccessfulAuth() {
+            this.$router.push({ path: "/profile" });
+        },
+        showErrorMessage(message) {
+            this.errorMsg = message;
+            this.modals.classic = true;
+        },
+        blockForm(value) {
+            this.isFormSubmitted = value;
+        },
+    },
+}
